e261763dc354a453963288a050f83a3f97e3a1fb
vanshikaChhikaraGit <vanshikachhikaraofficial@gmail.com>
2025-05-26 13:55:15 +0530

    added clerk webhook for database sync upon signup

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,29 @@
+import { db } from "@/server/db";
+
+export const POST = async (req: Request) => {
+
+    try {
+    const { data } = await req.json();
+  const email = data.email_addresses[0].email_address;
+  const firstName = data.first_name;
+  const lastName = data.last_name;
+  const imageUrl = data.image_url;
+  const id = data.id;
+
+  await db.user.create({
+    data: {
+      emailAddress: email,
+      firstName,
+      lastName,
+      imageUrl,
+      id,
+    },
+  });
+  console.log('user created')
+  return new Response('Webhook Recieved',{ status:200 })
+    } catch (error) {
+        console.log('error occured in syncing user to database clerk webhook',error)
+        return new Response('Error occured in clerk webhook',{ status:500 })
+    }
+
+};
