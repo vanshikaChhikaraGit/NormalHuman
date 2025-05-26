90321809264115eaca6eafe6a3b43bd88d710472
vanshikaChhikaraGit <vanshikachhikaraofficial@gmail.com>
2025-05-26 15:32:11 +0530

    modified schema to resolve id conflict

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -3,22 +3,19 @@ import { db } from "@/server/db";
 export const POST = async (req: Request) => {
 
     try {
-    const { data } = await req.json();
-  const email = data.email_addresses[0].email_address;
-  const firstName = data.first_name;
-  const lastName = data.last_name;
-  const imageUrl = data.image_url;
-  const id = data.id;
+   const { data } = await req.json();
+    const emailAddress = data.email_addresses[0].email_address;
+    const firstName = data.first_name;
+    const lastName = data.last_name;
+    const imageUrl = data.image_url;
+    const id = data.id;
 
-  await db.user.create({
-    data: {
-      emailAddress: email,
-      firstName,
-      lastName,
-      imageUrl,
-      id,
-    },
-  });
+    await db.user.upsert({
+        where: { id },
+        update: { emailAddress, firstName, lastName, imageUrl },
+        create: { id, emailAddress, firstName, lastName, imageUrl },
+    });
+
   console.log('user created')
   return new Response('Webhook Recieved',{ status:200 })
     } catch (error) {
